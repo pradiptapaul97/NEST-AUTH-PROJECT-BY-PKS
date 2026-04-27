dfe5d2872c72bd2c869f3228ce84443cb1ad5142
pradipta <ppradipta65@gmail.com>
2026-04-28 01:46:48 +0530

    DB setup

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsString, IsNotEmpty, IsBoolean, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'First name must be a string' })
+  @IsNotEmpty({ message: 'First name is required' })
+  firstName: string;
+
+  @IsString({ message: 'Last name must be a string' })
+  @IsNotEmpty({ message: 'Last name is required' })
+  lastName: string;
+
+  @IsBoolean({ message: 'isActive must be a boolean value' })
+  @IsOptional()
+  isActive?: boolean;
+}
+
+
