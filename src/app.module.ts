f257261188445ca762e814084b825b9446234b45
pradipta <ppradipta65@gmail.com>
2026-04-28 01:40:42 +0530

    DB setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'pradipta',
+      password: 'password',
+      database: 'mydb',
+      autoLoadEntities: true,
+      synchronize: true,//for dev only not in production use migrations in production
+      logging: true, // show sql queries in console
+      logger: 'formatted-console',// for better formatting of sql queries
+      poolSize: 5,// number of connections in the pool
+      maxQueryExecutionTime: 2000, // log queries that take longer than this time(optional)
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
